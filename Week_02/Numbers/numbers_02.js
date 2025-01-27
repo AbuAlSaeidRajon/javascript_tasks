9b1c8dc49c767c627f3735c94e0b00cd1dd66537
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-01-27 12:58:08 +0200

    numbers added

---
+++ Week_02/Numbers/numbers_02.js
@@ -0,0 +1,17 @@
+/* Create a variable price and assign it the value 19.56789.
+Use .toFixed() to:
+Round the price to 2 decimal places.
+Round the price to no decimal places.
+Print both results using console.log() with messages:
+"Price with 2 decimals: 19.57"
+"Price with no decimals: 20"
+ */
+
+let price = 19.56789;
+
+let price2Decimal = price.toFixed(2);
+
+let priceNoDecimal = price.toFixed(0);
+
+console.log("Price with 2 decimal is " + price2Decimal);
+console.log("Price with no decimal is " + priceNoDecimal);
---
+++ Week_02/Numbers/numbers_03.js
@@ -0,0 +1,23 @@
+/* 
+
+Create two variables:
+num1 with the value "10".
+num2 with the value "notANumber".
+Convert num1 and num2 using parseFloat().
+Print the sum of num1 and num2 using console.log().
+(Note: If one value is NaN, the result will be NaN, which will be printed automatically.)
+Example output:
+NaN (if the addition involves invalid input).
+10 (if both inputs are valid numbers).
+
+*/
+
+let num1 = "10";
+let num2 = "notANumber";
+
+let convertedNum1 = parseFloat(num1);
+let convertedNum2 = parseFloat(num2);
+
+let sum = convertedNum1 + convertedNum2;
+
+console.log("The sum is: " + sum);
