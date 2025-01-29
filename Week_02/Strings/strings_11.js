6b7eeb0c5a1ec37c553579a5c007da715a21044d
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-01-30 01:07:55 +0200

    string exercise 11 complete

--- Week_02/Strings/strings_11.js
+++ Week_02/Strings/strings_11.js
@@ -4,3 +4,9 @@ Print the result using console.log().
 Example output:
 "Does the phrase include 'powerful'? true"
  */
+
+let phrase = "JavaScript is versatile and powerful.";
+
+let includesPowerful = phrase.includes("powerful");
+
+console.log(`Does the phrase include 'powerful'? ${includesPowerful}`);
