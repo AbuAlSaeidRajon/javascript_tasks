9bb84c105128b324c9ffd83b4fb45c7e92287ae8
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-02-03 20:41:17 +0200

    condition 1 exercise complete

--- Week_02/Conditions/conditions_01.js
+++ Week_02/Conditions/conditions_01.js
@@ -6,3 +6,14 @@ Otherwise, return nothing.
 Call the function with different ages and print the result.
  */
 
+function checkAdult(age) {
+    if (age >= 18) {
+        return "You are an adult.";
+    }
+    return;
+}
+
+console.log(checkAdult(20)); // Should print: "You are an adult."
+console.log(checkAdult(17)); // Should print: undefined
+console.log(checkAdult(18)); // Should print: "You are an adult."
+console.log(checkAdult(15)); // Should print: undefined
