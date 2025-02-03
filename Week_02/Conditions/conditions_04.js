cb9fe5b60af0c7e0f740a736e6bb70c4d3db11de
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-02-03 23:10:16 +0200

    conditon exercise 4 omplete

--- Week_02/Conditions/conditions_04.js
+++ Week_02/Conditions/conditions_04.js
@@ -6,3 +6,16 @@ If either is false, return "You need to find food!"
 Call the function with different combinations of isHungry and hasFood. 
 */
 
+function canEat (isHungry, hasFood) {
+    if (isHungry && hasFood) {
+        return "Time to eat!";
+    } else {
+        return "You need to find food!"
+    }
+}
+
+// Call the function with different combinations of isHungry and hasFood and print the result
+console.log(canEat(true, true));   // Should print: "Time to eat!"
+console.log(canEat(true, false));  // Should print: "You need to find food!"
+console.log(canEat(false, true));  // Should print: "You need to find food!"
+console.log(canEat(false, false)); // Should print: "You need to find food!"
