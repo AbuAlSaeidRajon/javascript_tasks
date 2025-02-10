850ddefa1ecee9555b1997814c6101e1361ef525
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-02-10 13:16:08 +0200

    basic dom 1 complete

--- Week_04/Basic_DOM/Basic_DOM_01/script.js
+++ Week_04/Basic_DOM/Basic_DOM_01/script.js
@@ -3,3 +3,14 @@ Use an HTML file with a <p> element that has the ID content.
 Write JavaScript to select this element using getElementById.
 Create a function that changes its text to "Hello, World!" when a button is clicked.
 */
+
+// Get the <p> element with the ID content
+const  contentElement = document.getElementById("content");
+
+function changeText() {
+    contentElement.textContent = "Hello, World!";
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", changeText);
